23ad512abe6f68b0865c2d60b382ba4e41e6bd91
Adalberto Teixeira <adalbertosteixeira@gmail.com>
2017-12-15 11:11:06 +0000

    add better error messages

--- tests/lib/rules/no-global-lodash.js
+++ tests/lib/rules/no-global-lodash.js
@@ -17,40 +17,57 @@ var rule = require("../../../lib/rules/no-global-lodash"),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2017, sourceType: 'module' } });
 ruleTester.run("no-global-lodash", rule, {
 
     valid: [
       {
         code: "import get from 'lodash.get'",
-        parserOptions: { sourceType: 'module', ecmaVersion: 6 },
       }
     ],
 
     invalid: [
         {
-            code: "import { get, isEqual, cloneDeep } from 'lodash'",
-            parserOptions: { sourceType: 'module', ecmaVersion: 6 },
-            errors: [{
-                message: 'Expected global "lodash" not to be imported.',
-                type: 'ImportDeclaration',
-            }]
+          code: "import { get, isEqual, cloneDeep } from 'lodash'",
+          errors: [{
+            message: 'Expected get to be imported as "import get from \'lodash.get\'".',
+            type: 'ImportDeclaration',
+          }, {
+            message: 'Expected isEqual to be imported as "import isEqual from \'lodash.isequal\'".',
+            type: 'ImportDeclaration',
+          }, {
+            message: 'Expected cloneDeep to be imported as "import cloneDeep from \'lodash.clonedeep\'".',
+            type: 'ImportDeclaration',
+          }]
         },
         {
-            code: "import _, { get, isEqual, cloneDeep } from 'lodash'",
-            parserOptions: { sourceType: 'module', ecmaVersion: 6 },
-            errors: [{
-                message: 'Expected global "lodash" not to be imported.',
-                type: 'ImportDeclaration',
-            }]
+          code: `
+              import _, {
+                  get,
+                  isEqual,
+                  cloneDeep
+              } from 'lodash'
+          `,
+          errors: [{
+            message: 'Expected global "lodash" not to be imported.',
+            type: 'ImportDeclaration',
+          }, {
+            message: 'Expected get to be imported as "import get from \'lodash.get\'".',
+            type: 'ImportDeclaration',
+          }, {
+            message: 'Expected isEqual to be imported as "import isEqual from \'lodash.isequal\'".',
+            type: 'ImportDeclaration',
+          }, {
+            message: 'Expected cloneDeep to be imported as "import cloneDeep from \'lodash.clonedeep\'".',
+            type: 'ImportDeclaration',
+          }]
         },
         {
-            code: "import _ from 'lodash'",
-            parserOptions: { sourceType: 'module', ecmaVersion: 6 },
-            errors: [{
-                message: 'Expected global "lodash" not to be imported.',
-                type: 'ImportDeclaration',
-            }]
+          code: "import _ from 'lodash'",
+          errors: [{
+            message: 'Expected global "lodash" not to be imported.',
+            type: 'ImportDeclaration',
+          }]
         }
     ]
 });
